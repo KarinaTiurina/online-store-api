ce471e0376992c59b93bb2e87a13a44ea1f9b540
KarinaTiurina <rktiurina@gmail.com>
2018-12-02 18:00:45 +0300

    update products api

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,7 +2,12 @@ module Api
   class ProductsController < ApplicationController
     def index
       products = Product.all
-      render json: {data: products}, include: 'images', status: :ok
+      render json: products, include: 'images', status: :ok
+    end
+
+    def show
+      product = Product.find(params[:id])
+      render :json => product.to_json(:include => [:images])
     end
   end
 end
