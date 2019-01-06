a1a15f905aad44aa60d7f72d678199e2f5855e4a
KarinaTiurina <rktiurina@gmail.com>
2019-01-06 23:26:16 +0300

    orders post

---
+++ app/controllers/api/orders_controller.rb
@@ -0,0 +1,27 @@
+module Api
+  class OrdersController < ApplicationController
+    def create
+      order = Order.new(
+        full_name: params[:fullName],
+        phone: params[:phone],
+        email: params[:email],
+        address: params[:address]
+      )
+
+      if order.save && params[:basketItems].count > 0
+        params[:basketItems].each do |item|
+          OrderProduct.create(
+            product: Product.find(item[:product_id]),
+            order: order,
+            count: item[:count],
+            price_per_product: item[:price_per_product]
+          )
+        end
+
+        render json: {"result" => "OK"}, status: :ok
+      else
+        render json: {"message" => "Error"}, status: :error
+      end
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181202105206) do
+ActiveRecord::Schema.define(version: 20190105171532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,26 @@ ActiveRecord::Schema.define(version: 20181202105206) do
     t.index ["product_id"], name: "index_images_on_product_id"
   end
 
+  create_table "order_products", force: :cascade do |t|
+    t.bigint "order_id"
+    t.bigint "product_id"
+    t.integer "count"
+    t.float "price_per_product"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_products_on_order_id"
+    t.index ["product_id"], name: "index_order_products_on_product_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "full_name"
+    t.string "phone"
+    t.string "email"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.float "price"
@@ -31,4 +51,6 @@ ActiveRecord::Schema.define(version: 20181202105206) do
   end
 
   add_foreign_key "images", "products"
+  add_foreign_key "order_products", "orders"
+  add_foreign_key "order_products", "products"
 end
